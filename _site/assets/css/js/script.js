f58ab908390990bf54a0451b3fd2be1fa9884f59
karamo10 <pabimamie@gmail.com>
2024-12-09 22:27:06 +0000

    stylings updates

--- _site/assets/css/js/script.js
+++
@@ -1,6 +0,0 @@
-// const menuBtn = document.querySelector('#menu-btn');
-// const mobileMenu = document.querySelector('#mobile-menu');
-
-// menuBtn.addEventListener('click', () =>{
-//     mobileMenu.classList.toggle('hidden');
-// })
